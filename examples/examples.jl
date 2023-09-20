6b85b5db531bc6e544934213856cafe848116e59
Gage <gage.bonner@gmail.com>
2023-09-19 23:53:58 -0400

    examples

---
+++ examples/examples.jl
@@ -0,0 +1,15 @@
+"""
+    const EXAMPLE_DIST_APRIL_2018
+
+The path to a NetCDF file giving the April 2018 
+Sargassum distribution computed using `SargassumFromAFAI.jl`
+"""
+const EXAMPLE_DIST_APRIL_2018 = joinpath(@__DIR__, "dist-2018-4.nc")
+
+"""
+    const EXAMPLE_DIST_MAY_2018
+
+The path to a NetCDF file giving the May 2018 
+Sargassum distribution computed using `SargassumFromAFAI.jl`
+"""
+const EXAMPLE_DIST_MAY_2018 = joinpath(@__DIR__, "dist-2018-5.nc")
--- src/SargassumFromAFAI.jl
+++ src/SargassumFromAFAI.jl
@@ -6,8 +6,10 @@ export distribution_to_nc, afai_to_distribution
 export plot
 export download_data, data_path, data_rm
 export TREF, time2months, months2time
+export EXAMPLE_DIST_APRIL_2018, EXAMPLE_DIST_MAY_2018
 
 include(joinpath(@__DIR__, "main.jl"))
 include(joinpath(@__DIR__, "..", "data", "remote-files.jl"))
+include(joinpath(@__DIR__, "..", "examples", "examples.jl"))
 
 end # module
